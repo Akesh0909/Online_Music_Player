ff81041d022e65e56ff896e092d27e26bcc2d758
Akesh <akeshrao0909a.r@gmail.com>
2021-10-31 09:58:29 +0530

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "GYM de Shokeen ",
+    artist: "Bhalwaan",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Naam Tera",
+    artist: "Ndee Kundu",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Mohtarma",
+    artist: "Khasa Aala Chahar",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Devil",
+    artist: "Sidhu Moose Wala",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Shadow",
+    artist: "Singga",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "295",
+    artist: "Sidhu Moose Wala",
+    img: "music-6",
+    src: "music-6"
+  },
+ ];
